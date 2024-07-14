1d9a13911c4197fc19d9d6564ac8ea44137f0579
Aremu <aremu.script@gmail.com>
2024-07-14 08:02:07 +0100

    yooo

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://real-estate-server-delta.vercel.app/",
+  baseURL: "https://real-estate-server-delta.vercel.app",
   withCredentials: true,
 });
 
